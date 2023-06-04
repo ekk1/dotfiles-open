a2d81b46138158d7654d5df8026ec1c8abc32dfb
ekk1 <9464534+ekk1@users.noreply.github.com>
2023-06-04 16:29:08 +0800

    Create pacman.cmd

---
+++ pacman.cmd
@@ -0,0 +1,20 @@
+* Install
+pacman -S xxx
+
+* Search keyword
+pacman -Ss xxx
+
+* Query explicit install
+pacman -Qe
+
+* Query all install
+pacman -Qn
+
+* Query package file
+pacman -Ql xxx
+
+* Whole system upgrade
+pacman -Syu
+
+* Refresh metadata
+pacman -Sy
