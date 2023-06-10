a82b8572e8e8aaac35c2ecc52c6d1f411fb73498
ekk1 <9464534+ekk1@users.noreply.github.com>
2023-06-10 16:30:54 +0800

    Create init-archlinux.sh

---
+++ init-archlinux.sh
@@ -0,0 +1,8 @@
+pacman -S adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts
+pacman -S brightnessctl chromium chrony dhcpcd dmenu dolphin efibootmgr
+pacman -S firefox foot gammastep git grub htop i3blocks i3status
+pacman -S intel-gpu-tools intel-media-driver intel-ucode libva-utils
+pacman -S man-db mpv nfs-utils noto-fonts-cjk pavucontrol pipewire-pulse
+pacman -S polkit qt5-wayland qt5ct qt6-wayland ranger smartmontools sway
+pacman -S swaybg swayidle swaylock tmux tree ttf-ubuntu-mono-nerd upower
+pacman -S vim xorg-xwayland
