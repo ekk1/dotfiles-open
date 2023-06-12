b0dd7c8fd404cd06769bb4cf4d94947694b1816f
ekk1 <9464534+ekk1@users.noreply.github.com>
2023-06-12 17:15:19 +0800

    Update init-debian.sh

--- init-debian.sh
+++ init-debian.sh
@@ -1,1 +1,9 @@
+# Vim related
+apt install vim vim-nox vim-youcompleteme vim-ctrlp vim-airline vim-airline-themes vim-addon-manager vim-ale vim-python-jedi
+
+# Sway
+apt install sway swaybg swayidle swaylock sway-backgrounds foot dmenu wofi i3status i3blocks
+
+# Fonts
+apt install fonts-noto-cjk fonts-noto-mono fonts-noto-color-emoji fonts-noto-cjk-extra fonts-font-awesome fonts-powerline 
 
