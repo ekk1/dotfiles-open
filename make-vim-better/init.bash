c219a1bc0178197fe15eab19d4f9d0b5bc9babe0
Your Name <you@example.com>
2023-08-14 00:14:51 +0800

    [update] vim: remove auto pair && update init script

--- make-vim-better/init.bash
+++ make-vim-better/init.bash
@@ -1,13 +1,18 @@
 for ii in autoload doc plugin syntax
 do
-    mkdir -p .vim/$ii
+    mkdir -p ~/.vim/$ii
 done
 
-cp /usr/share/vim/vim82/syntax/markdown.vim .vim/syntax/markdown.vim
+cp /usr/share/vim/vim90/syntax/markdown.vim .vim/syntax/markdown.vim
 
 # change these lines
 vim .vim/syntax/markdown.vim
 # syn match markdownListMarker "\%(\t\| \{0,32\}\)[-*+]\%(\s\+\S\)\@=" contained
 # syn match markdownOrderedListMarker "\%(\t\| \{0,16}\)\<\d\+\.\%(\s\+\S\)\@=" contained
 
-
+# edit vimrc
+# change this line
+# vimJumpCacheLocation
+# CheetSheetLocation can be deleted
+#
+# use Space+m to save a file to avoid error
