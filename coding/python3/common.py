1ceaf47f9229a315e99679f038c9a43ef542c7e3
Your Name <aaa@aaa.com>
2023-08-28 17:34:38 +0800

    [update] python3: common func

--- coding/python3/common.py
+++ coding/python3/common.py
@@ -1,11 +1,18 @@
 import subprocess
 import typing
+import datetime
 
 def run_cmd(cmd: str, env: typing.Optional[dict]=None) -> subprocess.CompletedProcess:
     """run cmd"""
     return subprocess.run(
             ['bash', '-c' , cmd],
-            capture_output=True,
-            env=env
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            universal_newlines=True,
+            env=env,
     )
+
+def log_print(msg: str, level: typing.Optional[str]="INFO"):
+    '''print with level and date'''
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("[{0}][{1}]: {2}".format(now, level, msg))
 
