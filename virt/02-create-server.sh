3938ee8548c10f67ec7540ef5cc577beb00c978c
Your Name <aaa@aaa.com>
2023-08-31 17:00:26 +0800

    [update] qemu: better create

--- virt/02-create-server.sh
+++ virt/02-create-server.sh
@@ -1,11 +1,11 @@
 qemu-system-x86_64 \
+    -m 2G -smp 2 \
     -drive file=testvm-1.qcow2,if=virtio \
     -netdev user,id=netout,hostname=testvm-1,restrict=on,hostfwd=tcp:127.0.0.1:2222-:22 \
     -device virtio-net,netdev=netout \
     -netdev socket,id=netshare,listen=127.0.0.1:3333 \
     -device virtio-net,netdev=netshare \
     -device virtio-rng-pci \
-    -m 2G -smp 2 \
     -name "testvm-1" \
     -boot d -vnc 127.0.0.1:10 \
     -monitor tcp:127.0.0.1:6001,server,nowait \
@@ -14,11 +14,11 @@ qemu-system-x86_64 \
     -display none
 
 # Extra options
-    # -enable-kvm
+    # -enable-kvm \
+    # -m 32G -smp 8 \
     # -netdev socket,id=netshare,listen=127.0.0.1:3333 \
     # -netdev socket,id=netshare,connect=127.0.0.1:3333 \
     # -device virtio-net,netdev=netshare \
     # -monitor unix:/tmp/monitor.sock,server,nowait \
-    # -m 32G -smp 8 \
     # nc -U /tmp/monitor.sock
 
