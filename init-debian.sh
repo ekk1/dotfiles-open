f6c23328fa911dc9132b4b5e50bc146d47ae1fa6
Your Name <you@example.com>
2023-09-09 14:08:40 +0800

    [add] debian init

--- init-debian.sh
+++ init-debian.sh
@@ -2,22 +2,25 @@
 apt install vim vim-nox vim-youcompleteme vim-ctrlp vim-airline vim-airline-themes vim-addon-manager vim-ale vim-python-jedi
 
 # Sway
-apt install sway swaybg swayidle swaylock sway-backgrounds foot dmenu wofi i3status i3blocks
+apt install sway swaybg swayidle swaylock sway-backgrounds foot foot-themes dmenu wofi i3status i3blocks
 
 # Fonts
 apt install fonts-noto-cjk fonts-noto-mono fonts-noto-color-emoji fonts-noto-cjk-extra fonts-font-awesome fonts-powerline fonts-ubuntu-console
+apt install ncdu htop tmux netcat-openbsd wget curl iptables tig
+apt install gimp
 
 # Kernel building
-apt install pahole
-apt install build-essential binutils-dev libncurses5-dev libssl-dev ccache bison flex libelf-dev
-apt install bc debhelper rsync
-for i in ~/linux-surface/patches/[VERSION]/*.patch; do patch -p1 < $i; done
+apt install pahole build-essential binutils-dev libncurses5-dev libssl-dev ccache bison flex libelf-dev bc debhelper rsync
+for i in ~/linux-surface/patches/6.1/*.patch; do patch -p1 < $i; done
 ./scripts/kconfig/merge_config.sh <base-config> ~/linux-surface/configs/surface-5.10.confi
 make menuconfig
 make olddefconfig
 
 make -j `getconf _NPROCESSORS_ONLN` bindeb-pkg LOCALVERSION=-linux-surface
 dpkg -i linux-headers-[VERSION].deb linux-image-[VERSION].deb linux-libc-dev-[VERSION].deb
+
+# Check the readme
+dpkg -L wireless-regdb
 
 # Quick firewall
 cat << EOF > /home/xxxx/00-startup.sh
