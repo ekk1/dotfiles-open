571ec6410feac1ba37fd0bec1809a7e361d25b26
Your Name <you@example.com>
2023-09-13 21:11:02 +0800

    [add] some flags for ssh

--- virt/21-ssh-to-vm.sh
+++ virt/21-ssh-to-vm.sh
@@ -1,3 +1,7 @@
 ssh -R 9050 -p 2222 user@127.0.0.1
+# Outside HTTP Proxy
+# -R 8118:xxxx:8118
+# Local Port forward
+# -L 127.0.0.1:xxxx:127.0.0.1:xxxx
 # If outside socks5 proxy presents
 # ssh -R 9050:host:port -p 2222 user@127.0.0.1
