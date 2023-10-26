e2311d7124a63991ecc66343a1508c6e448c638c
Your Name <you@example.com>
2023-10-26 20:41:10 +0800

    fix

--- coding/openai/init.py
+++ coding/openai/init.py
@@ -12,7 +12,7 @@ if os.path.exists("io.txt"):
     prompt = commons.decode_io(dd)
 
 with open("io.txt", 'w', encoding="utf8") as f:
-    f.write(f"PROMPT: {prompt}\n")
+    f.write(f"PROMPT: {prompt[0]}\n")
     f.write(commons.SEPRATOR)
     f.write("USER: \n")
     f.write(commons.SEPRATOR)
