202e56f61dba22c15d7bc5c87d10670aec70dd5d
Your Name <you@example.com>
2023-10-26 20:39:10 +0800

    [add] openai: dont clear prompt

--- coding/openai/init-all.py
+++ coding/openai/init-all.py
@@ -6,13 +6,8 @@ if not os.path.exists("key.txt"):
 
 import commons
 
-if os.path.exists("io.txt"):
-    with open("io.txt", 'r', encoding='utf8') as f:
-        dd = f.read()
-    prompt = commons.decode_io(dd)
-
 with open("io.txt", 'w', encoding="utf8") as f:
-    f.write(f"PROMPT: {prompt}\n")
+    f.write("PROMPT: \n")
     f.write(commons.SEPRATOR)
     f.write("USER: \n")
     f.write(commons.SEPRATOR)
