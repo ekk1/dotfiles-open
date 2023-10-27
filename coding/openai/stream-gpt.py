4faf12563a78753af5c9a405ca5f486fb7950563
Your Name <you@example.com>
2023-10-27 10:46:13 +0800

    [update] openai: merge all functions

--- coding/openai/stream-gpt.py
+++ coding/openai/stream-gpt.py
@@ -1,20 +1,102 @@
 """quick talk"""
-import commons
 import string
 import sys
 import os
+import json
+import requests
+
+OPENAI_API_ENDPOINT="https://api.chatanywhere.cn"
+# OPENAI_API_ENDPOINT="https://api.chatanywhere.com.cn"
+SEPRATOR = "<<>>++__--!!@@##--<<>>\n"
+
+print("Using: ", OPENAI_API_ENDPOINT)
+
+def load_keys():
+    with open("key.txt", 'r', encoding='utf8') as f:
+        data = f.read()
+    return data.strip()
+
+def init_io_file():
+    pp = input("Prompt: ")
+    with open("io.txt", 'w', encoding="utf8") as f:
+        f.write(f"PROMPT: {pp}\n")
+        f.write(SEPRATOR)
+
+def list_models(k):
+    """list all models"""
+    header = {"Authorization": f"Bearer {k}"}
+    r = requests.get(f"{OPENAI_API_ENDPOINT}/v1/models", headers=header, timeout=10)
+    try:
+        aa = json.loads(r.text)
+        for model in aa['data']:
+            print(model['id'])
+    except:
+        print(r.text)
+
+def decode_io(data):
+    data_parts = data.split(SEPRATOR)
+    prompt_part = data_parts[0]
+    prompt = prompt_part.split("PROMPT: ")[1].strip()
+    ret = []
+    for parts in data_parts[1:]:
+        if parts.startswith("USER: "):
+            cc = parts.split("USER: ")[1].strip()
+            ret.append({"role": "user", "content": cc})
+        elif parts.startswith("ASSISTANT: "):
+            cc = parts.split("ASSISTANT: ")[1].strip()
+            ret.append({"role": "assistant", "content": cc})
+    return prompt, ret
+
+def talk_with_gpt4_streamed(k, prompt, msg, model):
+    if len(msg) == 0:
+        raise RuntimeError("Message cannot be empty array")
+    if msg[-1]['role'] != "user":
+        raise RuntimeError("Last messgae has to be user")
+    if msg[-1]['content'] == "":
+        raise RuntimeError("Message cannot be empty")
+    header = {"Authorization": f"Bearer {k}"}
+    message_list = []
+    if prompt != "":
+        message_list.append({"role": "system", "content": prompt})
+    for mm in msg:
+        message_list.append(mm)
+    print(message_list)
+    req_body = {
+        "model": model,
+        "messages": message_list,
+        "stream": True,
+    }
+    r = requests.post(
+        f"{OPENAI_API_ENDPOINT}/v1/chat/completions",
+        headers=header, json=req_body, timeout=600, stream=True,
+    )
+    all_ret = ""
+    for line in r.iter_lines():
+        if len(line) > 1 and "data" in line.decode():
+            try:
+                data = json.loads(line.decode()[5:])
+                if 'content' in data['choices'][0]['delta']:
+                    cc = data['choices'][0]['delta']['content']
+                    print(cc, end="", flush=True)
+                    all_ret += cc
+                else:
+                    print()
+            except Exception as e:
+                print("DONE")
+    return all_ret + "\n"
 
 if not os.path.exists("key.txt"):
     with open("key.txt", 'w', encoding="utf8") as f:
         f.truncate()
-
-print("Using: ", commons.OPENAI_API_ENDPOINT)
+    print("Please put the key in key.txt")
+    sys.exit(2)
 
 models = [
     "gpt-3.5-turbo-16k-0613",
     "gpt-4-0613",
 ]
 use_model = models[0]
+
 print("Choose model, default [0]:")
 for ii in range(0, len(models)):
     print(f"[{ii}]: {models[ii]}")
@@ -34,18 +116,18 @@ while True:
 print("Using model: ", use_model)
 
 if not os.path.exists("io.txt"):
-    commons.init_io_file()
+    init_io_file()
 
 print("[Input cc to clear context]")
 
 with open("io.txt", encoding="utf8") as f:
     data = f.read()
-prompt, dialogue = commons.decode_io(data)
+prompt, dialogue = decode_io(data)
 print("Using prompt: ", prompt)
 print("Previous dialogue: ")
 for d in dialogue:
     if len(d['content']) > 30:
-        print(d['role'].upper() + ": " + d['content'][:30])
+        print(d['role'].upper() + ": " + d['content'][:30] + "[TRUNCATED]")
     else:
         print(d['role'].upper() + ": " + d['content'])
 
@@ -53,10 +135,10 @@ while True:
     inp = input("USER: ")
     if inp == "cc":
         dialogue.clear()
-        commons.init_io_file()
+        init_io_file()
         with open("io.txt", encoding="utf8") as f:
             data = f.read()
-        prompt, dialogue = commons.decode_io(data)
+        prompt, dialogue = decode_io(data)
         print("Using prompt: ", prompt)
         continue
     if len(inp) == 0:
@@ -64,16 +146,10 @@ while True:
         continue
     dialogue.append({"role": "user", "content": inp})
     print("ASSISTANT: ", end="", flush=True)
-    reply = commons.talk_with_gpt4_streamed(commons.load_keys(), prompt, dialogue, use_model)
-    dialogue.append({"role": "assistant", "content": reply})
+    reply = talk_with_gpt4_streamed(load_keys(), prompt, dialogue, use_model)
+    dialogue.append({"role": "assistant", "content": reply.strip()})
     with open("io.txt", 'a+', encoding="utf8") as f:
-        f.write("USER: " + inp)
-        if len(inp) > 0 and inp[-1] != "\n":
-            f.write("\n")
-        f.write(commons.SEPRATOR)
-        f.write("ASSISTANT: " + reply)
-        if len(reply) > 0 and reply[-1] != "\n":
-            f.write("\n")
-        elif len(reply) == 0:
-            f.write("\n")
-        f.write(commons.SEPRATOR)
+        f.write("USER: " + inp.strip() + "\n")
+        f.write(SEPRATOR)
+        f.write("ASSISTANT: " + reply.strip() + "\n")
+        f.write(SEPRATOR)
