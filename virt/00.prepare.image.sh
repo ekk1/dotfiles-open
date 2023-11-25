3e1d04394c32b346576d035965a0177318a516f8
Your Name <you@example.com>
2023-11-25 14:12:52 +0800

    [update] virt

--- virt/00.prepare.image.sh
+++ virt/00.prepare.image.sh
@@ -1,5 +1,7 @@
 # wget https://mirrors.ustc.edu.cn/debian-cdimage/cloud/bookworm/20231013-1532/debian-12-nocloud-amd64-20231013-1532.qcow2
+# wget https://cloud.debian.org/images/cloud/bookworm/20231013-1532/debian-12-genericcloud-amd64-20231013-1532.qcow2
 # 33f0fea16bec9240686a3c8533670d1d7bb03eab58c0d628623d02dc8a871155a17464a17489260b127e18062ec2fb2ee64d17259f71093f7bf9465e6e7d1872  debian-12-nocloud-amd64-20231013-1532.qcow2
 # 1b91ebd04e31a687e8a419c409364a294f5475d50de62b168c89775e45dc472310bdddf547f8a216054f6b0f523bc87439c9a02f64bc841b3dc56cabfdc216ba  debian-12-nocloud-amd64-20231013-1532.raw
+# 6b55e88b027c14da1b55c85a25a9f7069d4560a8fdb2d948c986a585db469728a06d2c528303e34bb62d8b2984def38fd9ddfc00965846ff6e05b01d6e883bfe  debian-12-genericcloud-amd64-20231013-1532.qcow2
 #
 # qemu-img convert -f qcow2 -O raw img.qcow2 img.raw
--- virt/92-init-debian-root.sh
+++ virt/92-init-debian-root.sh
@@ -22,13 +22,16 @@ echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 echo "ja_JP.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
+apt install -y vim htop ncdu tmux git curl wget python3
+
+# apt install -y sysbench
+
+cat << EOF > 00-init-http-proxy.sh
 apt install -y privoxy
 echo "forward-socks5t / 127.0.0.1:9050 ." >> /etc/privoxy/config
 echo "debug 13313" >> /etc/privoxy/config
 systemctl restart privoxy
-
-apt install -y sysbench vim htop ncdu tmux git curl wget python3
 echo "alias terminalProxyStart='export http_proxy=http://127.0.0.1:8118; export https_proxy=http://127.0.0.1:8118; export all_proxy=http://127.0.0.1:8118'" >> /home/user/.bashrc
-
-echo "RUN terminalProxyStart !!!"
+echo "RUN terminalProxyStart using user !!!"
+EOF
 exit
