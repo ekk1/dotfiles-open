2b72db8fc4f83762c1e1498bed6a8ffee8ec64d4
Your Name <you@example.com>
2023-11-28 18:01:46 +0800

    [update] windows parts

--- virt/12.create.windows.sh
+++ virt/12.create.windows.sh
@@ -7,17 +7,15 @@ else
 fi
 
 qemu-system-x86_64 \
-    # -enable-kvm \
+    -enable-kvm \
     -m 2G -smp 2 \
-    -drive file=${disk_name},if=virtio \
-    -drive driver=raw,file=seed.iso,if=virtio \
-    -netdev user,id=netout,hostname=testvm-1,restrict=on,hostfwd=tcp:127.0.0.1:2222-:22 \
-    -device virtio-net,netdev=netout \
-    -netdev socket,id=netshare,listen=127.0.0.1:3333 \
-    -device virtio-net,netdev=netshare \
-    -device virtio-rng-pci \
-    -name "testvm-1" \
-    -boot d -vnc 127.0.0.1:10 \
+    -drive file=${disk_name} \
+    -cdrom /srv/vms/windows.iso \
+    -netdev user,id=netout,hostname=testvm-1,restrict=on,hostfwd=tcp:127.0.0.1:3389-:3389 \
+    -device e1000,netdev=netout \
+    -usb -device usb-tablet \
+    -name "testvmw-1" \
+    -boot menu=on -vnc 127.0.0.1:10 \
     -monitor tcp:127.0.0.1:6001,server,nowait \
     -serial tcp:127.0.0.1:5001,server,nowait \
     -daemonize \
