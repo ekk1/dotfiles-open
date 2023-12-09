ba542f0267446ee0c11e310612996a6c2728d1b7
Your Name <you@example.com>
2023-12-09 15:23:31 +0800

    [update]

--- virt/91.init.vm.sh
+++ virt/91.init.vm.sh
@@ -7,4 +7,5 @@ scp -P 2222 -i vm_key 94.init.debian.user.sh user@127.0.0.1:
 scp -P 2222 -i vm_key -r ../dots ../make-vim-better user@127.0.0.1:
 
 ssh -R 9050 -p 2222 debian@127.0.0.1 -i vm_key sudo bash 93.init.debian.root.sh
-ssh -R 9050 -p 2222 user@127.0.0.1 -i vm_key bash 94.init.debian.user.sh
+# Run this is init user for dev
+# ssh -R 9050 -p 2222 user@127.0.0.1 -i vm_key bash 94.init.debian.user.sh
