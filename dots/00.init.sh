60144c35fae2bf7572d0a0ab481bfb239ca5cbd9
Your Name <you@example.com>
2023-12-09 10:05:26 +0800

    [update] add init scripts

---
+++ dots/00.init.sh
@@ -0,0 +1,8 @@
+cat bashrc >> ~/.bashrc
+ssh-keygen
+cp ssh_config ~/.ssh/config
+cp tmux.conf ~/.tmux.conf
+cp visidatarc ~/.visidatarc
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
