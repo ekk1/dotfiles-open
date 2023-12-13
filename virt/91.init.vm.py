29402b123c52d6b84f9b37925a87769b3fd503d2
Your Name <you@example.com>
2023-12-13 16:28:27 +0800

    [fix] add proxy for apt

--- virt/91.init.vm.py
+++ virt/91.init.vm.py
@@ -21,7 +21,7 @@ pp.add_argument('-m', '--multi',    type=int, help="multiple")
 pp.add_argument('-a', '--apt',      type=str, help="apt host", default="mirrors.ustc.edu.cn")
 pp.add_argument('-d', '--dry',      action="store_true", help="show commands but not exec")
 pp.add_argument('-n', '--http',     action="store_true", help="use http for apt")
-pp.add_argument('-p', '--proxy',    action="store_true", help="install privoxy for http proxy")
+pp.add_argument('-p', '--proxy',    action="store_true", help="assume need proxy")
 pp.add_argument('-q', '--dev',      action="store_true", help="install dev env")
 aa = pp.parse_args()
 
@@ -33,6 +33,7 @@ if aa.multi is not None:
     _inner_addr_base = "192.168.199."
 
 _nohttps = " -n=1" if aa.http else ""
+_aptproxy = " -p=1" if aa.proxy else ""
 
 for _vm_no in range(0, _multi_qemu):
     run_cmd(f"ssh-keygen -f \"/home/$USER/.ssh/known_hosts\" -R \"[127.0.0.1]:{2221 + _vm_no}\"", dry_run=aa.dry)
@@ -42,10 +43,11 @@ for _vm_no in range(0, _multi_qemu):
     run_cmd(f"scp -P {2221 + _vm_no} -i vm_key 94.init.debian.user.sh user@127.0.0.1:", dry_run=aa.dry)
     run_cmd(f"scp -P {2221 + _vm_no} -i vm_key -r ../dots ../make-vim-better debian@127.0.0.1:", dry_run=aa.dry)
     run_cmd(f"scp -P {2221 + _vm_no} -i vm_key -r ../dots ../make-vim-better user@127.0.0.1:", dry_run=aa.dry)
+    init_cmd =  f"ssh -R 9050 -p {2221 + _vm_no} debian@127.0.0.1 -i vm_key sudo bash 93.init.debian.root.sh -a={aa.apt}{_nohttps}{_aptproxy}"
     if _inner_addr_base != "":
-        run_cmd(f"ssh -R 9050 -p {2221 + _vm_no} debian@127.0.0.1 -i vm_key sudo bash 93.init.debian.root.sh -a={aa.apt}{_nohttps} -i={_inner_addr_base}{_inner_addr_start + _vm_no}", dry_run=aa.dry)
+        run_cmd(f"{init_cmd} -i={_inner_addr_base}{_inner_addr_start + _vm_no}", dry_run=aa.dry)
     else:
-        run_cmd(f"ssh -R 9050 -p {2221 + _vm_no} debian@127.0.0.1 -i vm_key sudo bash 93.init.debian.root.sh -a={aa.apt}{_nohttps}", dry_run=aa.dry)
+        run_cmd(f"{init_cmd}", dry_run=aa.dry)
     if aa.proxy:
         print("Install privoxy")
     if aa.dev:
