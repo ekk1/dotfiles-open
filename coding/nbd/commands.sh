17f4c6bf2d62c5898c07323e1d255410c2b87ae1
Your Name <you@example.com>
2024-03-16 22:27:17 +0800

    [update] nbd doc

--- coding/nbd/commands.sh
+++ coding/nbd/commands.sh
@@ -1,13 +1,17 @@
-apt install python3-requests
+# Client parts
+
+apt install nbd-client
+modprobe nbd
 nbd-client -l 127.0.0.1
 nbd-client -name test-nbd 127.0.0.1 /dev/nbd0
 nbd-client -d /dev/nbd0
 
-gunicorn -w 1 'index:app'
-
+# Server parts
+apt install python3-requests
 apt install python3-flask
 apt install python3-eventlet
 apt install gunicorn
 
+gunicorn -w 1 'index:app'
 # Use eventlet to enable http keepalive
 gunicorn -w 1 -k eventlet 'index:create_index_app()'
