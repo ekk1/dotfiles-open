7d8fd74af35dab66691dc5ff29918956c6da9a62
Your Name <you@example.com>
2024-09-13 18:16:47 +0800

    [add] quicker vm scripts

---
+++ scripts/31.create.vm.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+set -o errexit
+#set -o xtrace
+
+if [[ -z $1 || -z $2 || -z $3 ]] ; then
+    echo "bash 31.create.vm.sh disk cdrom action"
+    echo "disk dont need qcow2 suffix"
+    echo "cdrom is a must"
+    echo "action: "
+    echo '    q: query'
+    echo '    v: vnc'
+    echo '    k: kill'
+    echo '    w: windows'
+    echo '    s: start'
+    exit 0
+fi
+
+if ps aux | grep qemu | grep testvm ; then
+    echo "Already started"
+    if [[ ! -z $3 ]]; then
+        if [[ $3 == "v" ]]; then
+            echo "connect to vnc"
+            vncviewer 127.0.0.1:5911
+            echo "Done"
+            exit 0
+            echo "This shouldn't happen..."
+        elif [[ $3 == "k" ]]; then
+            echo "Terminating"
+            pid=$(ps aux | grep qemu | grep testvm | awk '{print $2}')
+            kill $pid
+            echo "Done"
+            exit 0
+            echo "This shouldn't happen..."
+        elif [[ $3 == "ss" ]]; then
+            echo "connect to ssh"
+            echo "-------------RUN THIS IN VM-----------------"
+            echo "export https_proxy=http://127.0.0.1:8118"
+            echo "export http_proxy=http://127.0.0.1:8118"
+            echo "-------------RUN THIS IN VM-----------------"
+            ssh root@127.0.0.1 -p 2221 -R 127.0.0.1:8118:127.0.0.1:8118
+            echo "Done"
+            exit 0
+            echo "This shouldn't happen..."
+        else
+            echo "Not defined action: $3"
+            exit 0
+            echo "This shouldn't happen..."
+        fi
+    else
+        exit 0
+    fi
+else
+    if [[ ! -z $3 ]] ; then
+        if [[ $3 == "q" ]]; then
+            echo "Not started"
+            exit 0
+            echo "This shouldn't happen..."
+        elif [[ $3 == "s" || $3 == "w" ]] ; then
+            echo "Starting vm"
+        else
+            echo "Not defined action: $3"
+            exit 0
+            echo "This shouldn't happen..."
+        fi
+    fi
+fi
+
+if [[ ! -f $1 ]] ; then
+    echo "No disk file, creating"
+    qemu-img create -f qcow2 $1.qcow2 20G
+    chmod 600 $1.qcow2
+fi
+
+if [[ ! -f $2 ]] ; then
+    echo "ISO file not found!!!"
+    exit 1
+fi
+
+basic_params="qemu-system-x86_64"
+
+if [[ $3 == "w" ]] ; then
+    echo "Create windows"
+    basic_params+=" -m 8G -smp 4"
+    basic_params+=" -drive file=$1.qcow2"
+    basic_params+=" -cdrom $2"
+    basic_params+=" -netdev user,id=netout,hostname=testvmw,restrict=on,hostfwd=tcp:127.0.0.1:2231-:3389"
+    basic_params+=" -device e1000,netdev=netout"
+    basic_params+=" -usb -device usb-tablet"
+    basic_params+=" -machine pc-q35-7.2"
+    basic_params+=" -name \"testvmw\""
+    basic_params+=" -boot menu=on"
+else
+    echo "Create simple linux"
+    basic_params+=" -m 4G -smp 2"
+    basic_params+=" -drive file=$1.qcow2,if=virtio"
+    basic_params+=" -cdrom $2"
+    basic_params+=" -netdev user,id=netout,hostname=testvm,restrict=on,hostfwd=tcp:127.0.0.1:2221-:22"
+    basic_params+=" -device virtio-net,netdev=netout"
+    basic_params+=" -device virtio-rng-pci"
+    basic_params+=" -name \"testvm\""
+    basic_params+=" -boot d"
+fi
+
+basic_params+=" -vnc 127.0.0.1:11"
+basic_params+=" -monitor tcp:127.0.0.1:6001,server,nowait"
+basic_params+=" -serial tcp:127.0.0.1:5001,server,nowait"
+basic_params+=" -sandbox on"
+basic_params+=" -daemonize"
+basic_params+=" -display none"
+
+if [[ ! -z $4 ]] ; then
+    echo "Enable KVM"
+    basic_params+=" -enable-kvm -cpu host"
+fi
+
+if [[ ! -z $5 ]] ; then
+    echo "Dryrun"
+    echo "Running $basic_params"
+else
+    echo "Running $basic_params"
+    $basic_params
+fi
