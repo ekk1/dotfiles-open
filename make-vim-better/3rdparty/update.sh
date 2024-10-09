49894e4e6cc6480567769e81562bc9905a91399c
Your Name <you@example.com>
2024-10-09 09:09:18 +0800

    [add] vim 3rd party tools

---
+++ make-vim-better/3rdparty/update.sh
@@ -0,0 +1,36 @@
+rm -rf ale
+mkdir -p ale
+cp -r ../../../vimthings/ale/ale_linters    ale/
+cp -r ../../../vimthings/ale/autoload       ale/
+cp -r ../../../vimthings/ale/doc            ale/
+cp -r ../../../vimthings/ale/ftplugin       ale/
+cp -r ../../../vimthings/ale/lspconfig.vim  ale/
+cp -r ../../../vimthings/ale/lua            ale/
+cp -r ../../../vimthings/ale/plugin         ale/
+cp -r ../../../vimthings/ale/rplugin        ale/
+cp -r ../../../vimthings/ale/syntax         ale/
+cp -r ../../../vimthings/ale/README.md      ale/
+
+rm -rf ctrlp.vim
+mkdir -p ctrlp.vim
+cp -r ../../../vimthings/ctrlp.vim/autoload     ctrlp.vim/
+cp -r ../../../vimthings/ctrlp.vim/doc          ctrlp.vim/
+cp -r ../../../vimthings/ctrlp.vim/plugin       ctrlp.vim/
+cp -r ../../../vimthings/ctrlp.vim/readme.md    ctrlp.vim/
+
+rm -rf nerdtree
+mkdir -p nerdtree
+cp -r ../../../vimthings/nerdtree/autoload          nerdtree/
+cp -r ../../../vimthings/nerdtree/doc               nerdtree/
+cp -r ../../../vimthings/nerdtree/lib               nerdtree/
+cp -r ../../../vimthings/nerdtree/nerdtree_plugin   nerdtree/
+cp -r ../../../vimthings/nerdtree/plugin            nerdtree/
+cp -r ../../../vimthings/nerdtree/README.markdown   nerdtree/
+cp -r ../../../vimthings/nerdtree/syntax            nerdtree/
+
+rm -rf vim-airline
+mkdir -p vim-airline
+cp -r ../../../vimthings/vim-airline/autoload       vim-airline/
+cp -r ../../../vimthings/vim-airline/doc            vim-airline/
+cp -r ../../../vimthings/vim-airline/plugin         vim-airline/
+cp -r ../../../vimthings/vim-airline/README.md      vim-airline/
--- notes/tools.md
+++ notes/tools.md
@@ -5,4 +5,8 @@
 apt install imagemagick
 ## Fonts are usually under /usr/share/fonts
 convert -size 140x80 xc:none -font /path/to/font.ttc -pointsize 20 -fill "rgba(255,255,255,0.3)" -gravity center -draw "translate 0,0 rotate 45 text 0,0 'watermark text'" miff:- | composite -tile - input.jpg output.jpg
+
+
+# tig
+# :toggle wrap-lines
 ```
